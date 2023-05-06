707dd58c9c5a9c7d7ca0b7c014baf9ba5a8a7759
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2023-05-06 15:49:33 -0500

    Add files via upload

--- client/services/ped.lua
+++ client/services/ped.lua
@@ -6,3 +6,11 @@ function PedAPI.SetStatic(ped)
     FreezeEntityPosition(ped, true)
     SetBlockingOfNonTemporaryEvents(ped, true)
 end
+
+function PedAPI.ScenarioInPlace(ped, hash, time)
+    FreezeEntityPosition(ped, true)
+    TaskStartScenarioInPlace(ped, joaat(hash), time, true, false, false, false)
+    Wait(time)
+    ClearPedTasksImmediately(ped)
+    FreezeEntityPosition(ped, false)
+end

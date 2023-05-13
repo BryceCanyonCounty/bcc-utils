3343aa9124b5507aa7640bec2259692d3e127fc7
Andrew <10902965+AndrewR3K@users.noreply.github.com>
2023-05-12 18:43:21 -0700

    Added a 2 hour version cache to release checks to prevent 304 errors (#4)
    * Added a 2 hour version cache to release checks to prevent 304 errors
    
    * gitignore added
    
    * gitignore added
    
    * json fix
    
    * version bump

--- server/services/versioner.lua
+++ server/services/versioner.lua
@@ -1,70 +1,100 @@
 VersionerAPI = {}
-HasChecked = {}
+ActiveCache = {}
 
-local function _interalCall(resourcename, repo)
-    local current = {
-        version = GetResourceMetadata(resourcename, 'version')
+local function _setActiveCache(repo, alerts)
+    ActiveCache[repo] = {
+        alerts = alerts,
+        timestamp = os.time()
     }
+    SaveResourceFile(GetCurrentResourceName(), './server/cache/version.json', json.encode(ActiveCache))
+end
 
-    PerformHttpRequest('https://api.github.com/repos/' .. repo .. '/releases/latest', function(err, text, headers)
-        if err ~= 200 then
-            print('^1ERROR: Please try again later. Code (', err, ')', '[', repo, ']')
-            return
+local function _getCachedRelease(repo)
+    return ActiveCache[repo]
+end
+
+local function _loadActiveCache()
+    ActiveCache = json.decode(LoadResourceFile(GetCurrentResourceName(), './server/cache/version.json'))
+end
+
+local function _interalCall(resourcename, repo, cached)
+    if cached then
+        local cached_release = _getCachedRelease(repo)
+
+        if cached_release.alerts[1] then
+            cached_release.alerts[1] = '^6(CACHED)' .. cached_release.alerts[1]
         end
-
-
-        local response = json.decode(text)
-        local latest = {
-            url = response.html_url,
-            body = response.body,
-            version = response.tag_name
+    
+        for _, value in ipairs(cached_release.alerts) do
+            print(value)
+        end
+    else
+        local current = {
+            version = GetResourceMetadata(resourcename, 'version')
         }
-        local uptodate = false
-        local overdate = false
+        local alerts = {}
 
-        if current.version > latest.version then
-            overdate = true
-        elseif current.version < latest.version then
-            uptodate = false
-        else
-            uptodate = true
-        end
+        PerformHttpRequest('https://api.github.com/repos/' .. repo .. '/releases/latest', function(err, text, headers)
+            if err ~= 200 then
+                print('^1ERROR: Please try again later. Code (', err, ')', '[', repo, ']')
+                return
+            end
 
-        if uptodate then
-            print('^2✅Up to Date! ^5[' .. resourcename .. '] ^6(Current Version ' .. current.version .. ')^0')
-        elseif overdate then
-            print('^3⚠️Unsupported! ^5[' .. resourcename .. '] ^6(Version ' .. current.version .. ')^0')
-            print('^4Current Version ^2(' .. latest.version .. ') ^3<' .. latest.url .. '>^0')
-        else
-            print('^1❌Outdated! ^5[' .. resourcename .. '] ^6(Version ' .. current.version .. ')^0')
-            print('^4NEW VERSION ^2(' .. latest.version .. ') ^3<' .. latest.url .. '>^0')
-            print('^4CHANGELOG ^0\r\n' .. latest.body)
-        end
 
-        HasChecked[repo] = {
-            active = true,
-            timestamp = os.time()
-        }
-    end, 'GET', json.encode(payload), {
-        ['Content-Type'] = 'application/json'
-    })
+            local response = json.decode(text)
+            local latest = {
+                url = response.html_url,
+                body = response.body,
+                version = response.tag_name
+            }
+            local uptodate = false
+            local overdate = false
+
+            if current.version > latest.version then
+                overdate = true
+            elseif current.version < latest.version then
+                uptodate = false
+            else
+                uptodate = true
+            end
+
+            if uptodate then
+                alerts = {
+                    '^2✅Up to Date! ^5[' .. resourcename .. '] ^6(Current Version ' .. current.version .. ')^0'
+                }
+            elseif overdate then
+                alerts = {
+                    '^3⚠️Unsupported! ^5[' .. resourcename .. '] ^6(Version ' .. current.version .. ')^0',
+                    '^4Current Version ^2(' .. latest.version .. ') ^3<' .. latest.url .. '>^0'
+                }
+            else
+                alerts = {
+                    '^1❌Outdated! ^5[' .. resourcename .. '] ^6(Version ' .. current.version .. ')^0',
+                    '^4NEW VERSION ^2(' .. latest.version .. ') ^3<' .. latest.url .. '>^0',
+                    '^4CHANGELOG ^0\r\n' .. latest.body
+                }
+            end
+            _setActiveCache(repo, alerts)
+
+            for _, value in ipairs(alerts) do
+                print(value)
+            end
+        end, 'GET', json.encode(payload), {
+            ['Content-Type'] = 'application/json'
+        })
+    end
 end
 
 function VersionerAPI.checkRelease(resourcename, repo)
     repo = repo:gsub("https://github.com/", "")
 
-    if HasChecked[repo] == nil then
-            _interalCall(resourcename, repo)
-            HasChecked[repo] = {
-                active = true,
-                timestamp = os.time()
-            }
+    local cached_release = _getCachedRelease(repo)
+    if cached_release == nil then
+        _interalCall(resourcename, repo, false)
     else
-        local daysfrom = os.difftime(os.time(), HasChecked[repo]) / (24 * 60 * 60) -- seconds in a day
-        local wholedays = math.floor(daysfrom)
-        if wholedays >= 1 then
-            _interalCall(resourcename, repo)
-        end
+        local hours_from = os.difftime(os.time(), cached_release.timestamp) / (60 * 60) -- seconds in a day
+        local whole = math.floor(hours_from)
+        _interalCall(resourcename, repo, whole < 2) -- 2 hour cache
     end
 end
 
@@ -75,37 +105,39 @@ function VersionerAPI.checkFile(resourcename, repo)
         version = GetResourceMetadata(resourcename, 'version')
     }
     PerformHttpRequest('https://raw.githubusercontent.com/' .. cleanrepo .. '/main/version',
-    function(err, response, headers)
-        local v = response:match("<%d?%d.%d?%d.?%d?%d?>"):gsub("[<>]", "")
-        local latest = {
-            url = repo,
-            body = response,
-            version = v
-        }
-        local uptodate = false
-        local overdate = false
+        function(err, response, headers)
+            local v = response:match("<%d?%d.%d?%d.?%d?%d?>"):gsub("[<>]", "")
+            local latest = {
+                url = repo,
+                body = response,
+                version = v
+            }
+            local uptodate = false
+            local overdate = false
 
-        if current.version > latest.version then
-            overdate = true
-        elseif current.version < latest.version then
-            uptodate = false
-        else
-            uptodate = true
-        end
+            if current.version > latest.version then
+                overdate = true
+            elseif current.version < latest.version then
+                uptodate = false
+            else
+                uptodate = true
+            end
 
-        if uptodate then
-            print('^2✅Up to Date! ^5[' .. resourcename .. '] ^6(Current Version ' .. current.version .. ')^0')
-        elseif overdate then
-            print('^3⚠️Unsupported! ^5[' .. resourcename .. '] ^6(Version ' .. current.version .. ')^0')
-            print('^4Current Version ^2(' .. latest.version .. ') ^3<' .. latest.url .. '>^0')
-        else
-            print('^1❌Outdated! ^5[' .. resourcename .. '] ^6(Version ' .. current.version .. ')^0')
-            print('^4NEW VERSION ^2(' .. latest.version .. ') ^3<' .. latest.url .. '>^0')
+            if uptodate then
+                print('^2✅Up to Date! ^5[' .. resourcename .. '] ^6(Current Version ' .. current.version .. ')^0')
+            elseif overdate then
+                print('^3⚠️Unsupported! ^5[' .. resourcename .. '] ^6(Version ' .. current.version .. ')^0')
+                print('^4Current Version ^2(' .. latest.version .. ') ^3<' .. latest.url .. '>^0')
+            else
+                print('^1❌Outdated! ^5[' .. resourcename .. '] ^6(Version ' .. current.version .. ')^0')
+                print('^4NEW VERSION ^2(' .. latest.version .. ') ^3<' .. latest.url .. '>^0')
 
-            local cl = latest.body:gsub("<" .. current.version .. ">.*", "")
-            print('^CHANGELOG ^0\r\n' .. cl)
-        end
-    end, 'GET', json.encode(payload), {
-        ['Content-Type'] = 'application/json'
-    })
+                local cl = latest.body:gsub("<" .. current.version .. ">.*", "")
+                print('^CHANGELOG ^0\r\n' .. cl)
+            end
+        end, 'GET', json.encode(payload), {
+            ['Content-Type'] = 'application/json'
+        })
 end
+
+_loadActiveCache()

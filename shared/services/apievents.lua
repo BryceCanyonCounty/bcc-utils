abfd2c0646962e93ed1e4c58d4d04b5429a7d3e1
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2025-02-08 17:53:46 -0800

    revert netapi

--- shared/services/apievents.lua
+++ shared/services/apievents.lua
@@ -8,48 +8,25 @@ local function decompressIt(data)
     return json.decode(LibDeflate:DecompressDeflate(data) or '')
 end
 
-local function storeEventID(eventName)
-    local attempts = 0
-    local max = 20
-    local id = UUID4()
-
-    while netEventIDs[eventName] == id and attempts < max do
-        id = UUID4()
-        attempts = attempts + 1
-    end
-    
-    netEventIDs[eventName] = id
-
-    return id
-end
-
-local function getEventID(eventName)
-    return netEventIDs[eventName]
-end
-
 NetEventsAPI = {}
 
 function NetEventsAPI:TriggerNetEvent(eventName, targetSource, ...)
-    local obfID = getEventID(eventName)
-
     local args = {...}
     for i, arg in ipairs(args) do
         args[i] = compressIt(arg)
     end
 
     if IsDuplicityVersion() then
-        TriggerClientEvent(obfID, targetSource or -1, table.unpack(args))
+        TriggerClientEvent(eventName, targetSource or -1, table.unpack(args))
     else
-        TriggerServerEvent(obfID, table.unpack(args))
+        TriggerServerEvent(eventName, table.unpack(args))
     end
 end
 
 function NetEventsAPI:RegisterNetEvent(eventName, callback)
-    local obfID = storeEventID(eventName)
-
     if IsDuplicityVersion() then
-        RegisterServerEvent(obfID)
-        AddEventHandler(obfID, function(...)
+        RegisterServerEvent(eventName)
+        AddEventHandler(eventName, function(...)
             local args = {...}
             for i, arg in ipairs(args) do
                 args[i] = decompressIt(arg)
@@ -57,8 +34,8 @@ function NetEventsAPI:RegisterNetEvent(eventName, callback)
             callback(table.unpack(args))
         end)
     else
-        RegisterNetEvent(obfID)
-        AddEventHandler(obfID, function(...)
+        RegisterNetEvent(eventName)
+        AddEventHandler(eventName, function(...)
             local args = {...}
             for i, arg in ipairs(args) do
                 args[i] = decompressIt(arg)
